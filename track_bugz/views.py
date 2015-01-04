286a2a9cee26f0f32c5001eabb977fab2940c865
Radomir Wojcik <radzhome@gmail.com>
2015-01-04 13:31:03 -0500

    added todos

--- track_bugz/views.py
+++ track_bugz/views.py
@@ -2,21 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from models import Ticket
-# from serializers import TicketSerializer
-# from rest_framework import generics
-# from rest_framework import permissions
-#
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from rest_framework.reverse import reverse
-# from rest_framework import renderers
-# from rest_framework.response import Response
-# from rest_framework import viewsets
-# from rest_framework.decorators import detail_route
 
 
 def test_view(request):
     return HttpResponse("TODO, use admin interface.", content_type="text/plain")
+
+
+def get_tickets(request):
+    #TODO: need to be able to query
+    return HttpResponse("TODO,", content_type="text/plain")
 
 # TODO: create a view that lets you get request header
 #request.META['HTTP_AUTHORIZATION']
